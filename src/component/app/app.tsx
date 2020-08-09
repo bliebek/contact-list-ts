d6485306380bc14c41438b975d1cec2bd18e02ac
Bartosz Liebek <bliebek@gmail.com>
2020-08-09 22:38:12 +0200

    refactor: change app structure; remove dead code

--- src/component/app/app.tsx
+++ src/component/app/app.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import apiData from "./api";
-import PersonInfo from "./PersonInfo";
+import apiData from "./../../service/api/api";
+import PersonInfo from "./../person/info";
 
 function App() {
   const [data, setData] = React.useState([]);
@@ -9,7 +9,7 @@ function App() {
   //  TODO fetch contacts using apiData function, handle loading and error states
 
   return (
-    <div className="App">
+    <div className="app">
       <div className="selected">Selected contacts: {selected.length}</div>
       <div className="list">
         {data.map((personInfo) => (
